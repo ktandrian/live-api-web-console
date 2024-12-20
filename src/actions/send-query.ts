568e4b3643fa80d4e3941c9dc1edfd069a3809a5
ktandrian <kentandrian@google.com>
2024-12-20 12:36:17 +0700

    feat(actions): add error handling

--- src/actions/send-query.ts
+++ src/actions/send-query.ts
@@ -3,16 +3,30 @@
 import { detectIntent } from "@/lib/dialogflow-cx";
 
 export async function sendQuery(query: string) {
-  const result = await detectIntent({
-    query: {
-      content: query,
-      type: "text",
-    },
-    projectId: "kentandrian-dialogflow",
-    location: "asia-southeast1",
-    agentId: "de52b0b6-7c22-4fb2-a606-0d2f84f2487a",
-    sessionId: "de52b0b6-7c22-4fb2-a606-0d2f84f2487a",
-  });
-  console.log('result', result);
-  return result.agentResponses;
+  try {
+    const result = await detectIntent({
+      query: {
+        content: query,
+        type: "text",
+      },
+      projectId: "kentandrian-dialogflow",
+      location: "asia-southeast1",
+      agentId: "de52b0b6-7c22-4fb2-a606-0d2f84f2487a",
+      sessionId: "de52b0b6-7c22-4fb2-a606-0d2f84f2487a",
+    });
+    console.log("sendQuery:", result);
+    return {
+      success: true,
+      payload: result.agentResponses,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      payload: [
+        "Sorry, I don't have the answer right now.",
+        "Is there anything else I can help you with?",
+      ],
+    };
+  }
 }
